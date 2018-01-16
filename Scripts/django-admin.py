b9411ca8055b79f6cd6aa81df19a48b5299649b5
Pepa <pep@soukroma.cz>
2018-01-16 22:22:40 +0100

    My django app, first commit

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\pc\documents\visual studio 2017\projects\pythonapplication6\pythonapplication6\env_django\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
