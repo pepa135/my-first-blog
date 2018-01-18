d450cb9f30d681df2d110bc6faeb2f16b5ba5f7d
Pepa <pep@soukroma.cz>
2018-01-18 20:53:57 +0100

    removed files

--- Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\pc\documents\visual studio 2017\projects\pythonapplication6\pythonapplication6\env_django\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
